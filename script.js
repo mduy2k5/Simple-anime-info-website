3089d9a717940947a8b42302172ad6409b136441
mduy2k5 <159438388+mduy2k5@users.noreply.github.com>
2024-07-22 22:03:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 // Function when click at one anime
-function clicked(classs,imgs,names,content,backcolor,textcolor){
+function setdelay(){
+    console.log("scroll");
+}
+setTimeout(setdelay, 10000);
+function clicked(classs,imgs,names,content,backcolor,textcolor,leftm, topm,backimg){
     document.getElementById("sec2").style.display='flex';
     document.getElementById("sec2").style.backgroundColor = backcolor;
+        
+    //document.getElementById("sec2").style.backgroundImage = "url("+imgs+")";
     document.getElementById("content").style.display='none';
     const mainsec = document.createElement("div");
     mainsec.className = "mainsec";
     mainsec.id = "mainsec";
+    mainsec.style.backgroundImage = "url("+backimg+")";
     const image = document.createElement("img");
     image.src = imgs;
     mainsec.append(image);
     const secondsec = document.createElement('div');
     secondsec.className = "secondsec";
     secondsec.id = "secondsec";
+    secondsec.style.display = "flex";
+    secondsec.style.marginLeft= leftm;
+    secondsec.style.marginTop = topm;
+    console.log(topm);
     const text1 = document.createElement('h3');
     text1.innerText = names;
     text1.id = "contenttext1";
@@ -21,26 +32,42 @@ function clicked(classs,imgs,names,content,backcolor,textcolor){
     text2.textContent = content;
     text2.id = "contenttext2";
     text2.className = "contenttext2";
+    text2.style.textShadow = "1px 1px "+textcolor;
     secondsec.append(text1);
     secondsec.append(text2);
     const btn = document.createElement('button');
     btn.className = "btn1";
     btn.id = "btn1";
-    btn.onclick = function() {start1();};
-    btn.innerText = "Back";
+    //btn.onclick = function() {start2();};
+    btn.innerText = "Watch";
     btn.style.backgroundColor = textcolor;
     secondsec.append(btn);
     mainsec.appendChild(secondsec);
+    
+    
     document.getElementById("sec2").append(mainsec);
-    window.scrollTo(0, window.innerHeight);
+    window.scrollTo({ top: 145, behavior: 'smooth' });
+    // let i = 0;
+    // while (i< 145){
+    //     window.scrollTo(0,i);
+    //     setdelay();
+    //     i = i+1;
+    // }
+    // const lop = (i) => {
+    //     setTimeout(()=> {
+    //         i+= 1;
+    //         window.scrollTo(0,i);
+    //         if (i< 145) lop(i);
+    //     },15);
+    // };
+    // lop(i);
+    
 }
+
 // Function after login
 function start(){
     document.getElementById("start").style.opacity=0;
-    const winheight = String(window.innerHeight) + "px";
-    document.getElementById('start').style.height = winheight;
-    const winheightneg = String(-window.innerHeight) + "px";
-    document.getElementById("start").style.marginTop= winheightneg;
+    document.getElementById("start").style.marginTop='-900px';
     document.getElementById("nav-links").style.display='flex';
     document.getElementById("s003").style.display='flex';
     document.getElementById("content").style.display='flex';
@@ -49,8 +76,7 @@ function start(){
 // Function back from info page
 function start1(){
     document.getElementById("start").style.opacity=0;
-    const winheightneg = String(-window.innerHeight) + "px";
-    document.getElementById("start").style.marginTop=winheightneg;
+    document.getElementById("start").style.marginTop='-900px';
     document.getElementById("nav-links").style.display='flex';
     document.getElementById("s003").style.display='flex';
     document.getElementById("content").style.display='flex';
