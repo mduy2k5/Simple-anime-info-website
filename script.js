7f62c67c22a15164f35fc0e0b212f3efb78ad5f4
mduy2k5 <maiduy2k5@gmail.com>
2025-08-19 22:37:17 +0700

    19/8/2025 index.html

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function clicked(classs,imgs,names,content,backcolor,textcolor){
     document.getElementById("sec2").style.display='flex';
     document.getElementById("sec2").style.backgroundColor = backcolor;
     document.getElementById("content").style.display='none';
+    document.getElementById("content_header").style.display='none';
+    document.getElementById("anime-trending").style.display='none';
+
     const mainsec = document.createElement("div");
     mainsec.className = "mainsec";
     mainsec.id = "mainsec";
@@ -34,6 +37,45 @@ function clicked(classs,imgs,names,content,backcolor,textcolor){
     document.getElementById("sec2").append(mainsec);
     window.scrollTo(0, window.innerHeight);
 }
+function top_anime() {
+    fetch('https://api.jikan.moe/v4/top/anime')
+    .then((response) => response.json())
+    .then((json) => { 
+        let count = 1;
+        console.log(json);
+        json.data.forEach(anime => {
+            if (count > 10) return; // Chỉ lấy 10 anime đầu tiên
+            const big_div = document.createElement('div');
+            big_div.className = "swiper-slide";
+            big_div.id = "swiper-slide";
+            const item_div = document.createElement('div');
+            item_div.className = "item";
+            const number_div = document.createElement('div');
+            number_div.className = "number";
+            const span_ele = document.createElement('span');
+            span_ele.textContent = count;
+            number_div.append(span_ele);
+            const text_ele = document.createElement('div');
+            text_ele.className = "film-title-dynamic-name";
+            text_ele.textContent = anime.title;
+            number_div.append(text_ele);
+            item_div.append(number_div);
+            const image = document.createElement('img');
+            image.src = anime.images.jpg.image_url;
+            image.className = "film-poster";
+            item_div.append(image);
+            const clear_fix = document.createElement('div');
+            clear_fix.className = "clearfix";
+            item_div.append(clear_fix);
+            big_div.append(item_div);
+            document.getElementById("swiper-wrapper").append(big_div);
+            count++;
+        });
+    })
+    .catch((error) => {
+        console.error('Error fetching data:', error);
+    });
+}
 // Function after login
 function start(){
     document.getElementById("start").style.opacity=0;
@@ -44,7 +86,8 @@ function start(){
     document.getElementById("nav-links").style.display='flex';
     document.getElementById("s003").style.display='flex';
     document.getElementById("content").style.display='flex';
-    
+    document.getElementById("anime-trending").style.display = 'block';
+    document.getElementById("content_header").style.display='flex';
 }
 // Function back from info page
 function start1(){
@@ -63,3 +106,45 @@ function start1(){
         console.log("Error");
     }
 }
+// first load
+function firstrender() {
+    // call ApI
+    fetch('https://api.jikan.moe/v4/seasons/now')
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json);
+        json.data.slice(0,21).forEach(anime => {
+            const node = document.createElement('li');
+            node.className = anime.mal_id;
+            const image = document.createElement('img');
+            image.src = anime.images.jpg.image_url;
+            node.append(image);
+            const text = document.createElement('h4');
+            text.textContent = anime.title;
+            node.appendChild(text);
+            node.onclick = function () { 
+                fetch('https://api.jikan.moe/v4/anime/'+ anime.mal_id +'/full')
+                .then((response) => response.json())
+                .then((json) => {
+                    clicked(json.data.mal_id,json.data.images.jpg.large_image_url,json.data.title,json.data.synopsis,"#ffffff","#000000");
+                })
+                .catch((error) => {
+                    console.error('Error fetching data:', error);
+                });
+                fetch('https://api.jikan.moe/v4/anime/'+ anime.mal_id +'/pictures')
+                .then((response) => response.json())
+                .then((json) => {
+                    console.log(json);
+                })
+                .catch((error) => {
+                    console.error('Error fetching data:', error);
+                });
+            };
+            document.getElementById("content").appendChild(node);
+        });
+    })
+    .catch((error) => {
+        console.error('Error fetching data:', error);
+    });
+}
+
